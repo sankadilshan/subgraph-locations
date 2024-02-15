61485108ffd4442be7df76ab1af6c8ec4a9b6066
sankadilshan <38968983+sankadilshan@users.noreply.github.com>
2024-02-15 08:40:36 +0530

    Revert "resolver reference"

--- resolvers.js
+++ resolvers.js
@@ -7,11 +7,6 @@ const resolvers = {
       return dataSources.locationsAPI.getLocation(id);
     },
   },
-  Location: {
-    __resolveReference: ({id}, {dataSources}) => {
-      return dataSources.locationsAPI.getLocation(id);
-   },
-  }
 };
 
 module.exports = resolvers;
